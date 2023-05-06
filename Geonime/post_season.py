14c36bda258938db0ada7012a1d90df47c6bcc5c
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-05-06 23:49:35 +0700

    Update post_season.py

--- Geonime/post_season.py
+++ Geonime/post_season.py
@@ -5,6 +5,20 @@ from bot import app
 
 @Client.on_message(filters.command("post_season"))
 async def start(client, message):
-  title = await message.chat.ask('Masukkan Judul:')
-  studio = await message.chat.ask('Masukkan Studio:')
-  await message.reply(f'Judul: {title.text}\nStudio: {studio.text}')
+  title = await message.chat.ask("Input Judul:")
+  synopsis = await message.chat.ask("Input Synopsis:")
+  types = await message.chat.ask("Input types:")
+  studio = await message.chat.ask("Input Studios:")
+  premiered = await message.chat.ask("Input Premiered:")
+  dateaired = await message.chat.ask("Input Date aired:")
+  
+  eps1 = await message.chat.ask("Input Episode 1:")
+  eps2 = await message.chat.ask("Input Episode 2:")
+  eps3 = await message.chat.ask("Input Episode 3:")
+  
+  if eps1:
+    await message.reply(f'Judul: {title.text}\nEpisode1: {studio.text}')
+  if eps2:
+    await message.reply(f'Judul: {title.text}\nEpisode1: {studio.text}\nEpisode2: {studio.text}')
+  if eps3:
+    await message.reply(f'Judul: {title.text}\nEpisode1: {studio.text}\nEpisode2: {studio.text}\nEpisode3: {studio.text}')
