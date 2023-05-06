c38203a98f6533f8937b9efb2b6d630da3188f51
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-05-06 23:38:07 +0700

    Update and rename start.py to post_season.py

--- Geonime/post_season.py
+++ Geonime/post_season.py
@@ -5,6 +5,6 @@ from bot import app
 
 @Client.on_message(filters.command("post_season"))
 async def start(client, message):
-  title = await message.chat.ask('Masukkan Judul:', parse_mode=enums.ParseMode.MARKDOWN)
-  studio = await message.chat.ask('Masukkan Studio:', parse_mode=enums.ParseMode.MARKDOWN)
+  title = await message.chat.ask('Masukkan Judul:')
+  studio = await message.chat.ask('Masukkan Studio:')
   await message.reply(f'Judul: {title.text}\nStudio: {studio.text}')
