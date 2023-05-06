01e841ba92c46707a81d93731925466992146d92
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-05-06 23:20:55 +0700

    Update start.py

--- Geonime/start.py
+++ Geonime/start.py
@@ -6,3 +6,6 @@ from bot import app
 @Client.on_message(filters.command("start"))
 async def start(client, message):
   await message.reply("Halo !")
+  answer = await message.chat.ask('*Send me your name:*', parse_mode=enums.ParseMode.MARKDOWN)
+  await answer.request.edit_text("Name received!")
+  await answer.reply(f'Your name is: {answer.text}', quote=True)
