afc35ff4bc9e8ddfab1f6375fa6c8f8e8ea8c7c2
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-05-07 00:16:24 +0700

    Update post_season.py

--- Geonime/post_season.py
+++ Geonime/post_season.py
@@ -16,17 +16,17 @@ async def start(client, message):
   eps1 = await message.chat.ask("Input Episode 1:")
   if not eps1 == "-":
       await message.reply(f'Judul: {title.text}\nEpisode1: {eps1.text}')
-  else None
+  else: None
       
   
   eps2 = await message.chat.ask("Input Episode 2:")
   if not eps2 == "-":
       await message.reply(f'Judul: {title.text}\nEpisode1: {eps1.text}\nEpisode2: {eps2.text}')
-  else None
+  else: None
      
   
   eps3 = await message.chat.ask("Input Episode 3:")
   if not eps3 == "-":
       await message.reply(f'Judul: {title.text}\nEpisode1: {eps1.text}\nEpisode2: {eps2.text}\nEpisode3: {eps3.text}')
-  else None
+  else: None
       
