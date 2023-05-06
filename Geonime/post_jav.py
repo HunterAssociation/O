acf46b34cd4f69eb99303c5b2c893f53f3a5c46c
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-05-07 03:17:57 +0700

    Update post_jav.py

--- Geonime/post_jav.py
+++ Geonime/post_jav.py
@@ -1,79 +1,66 @@
-from pyrogram import Client, filters, enums
-from pyrogram.types import *
-from bot import app
+<span><!--more--></span>
+<!--[ Thumbnail ]-->
 
 
-@Client.on_message(filters.command("jav"))
-async def start(client, message):
-  photo = await message.chat.ask("Input Photo (Url):")
-  server1 = await message.chat.ask("Input Video Server 1:")
-  server2 = await message.chat.ask("Input Video Server 2:")
- 
-  TEXT_MSG = f"""
-  <span><!--more--></span>
-  <!--[ Thumbnail ]-->
-  <div class="separator" style="clear: both;"><a href="{photo}" style="display: block; padding: 1em 0; text-align: center; "><img alt="" border="0" data-original-height="600" data-original-width="424" src="{photo}"/></a></div>
-  <div class="server_option" style="font-family:'Poppins', sans-family;font-weight:100px;">
-    <center>If the video doesn't appear<br/>refresh the page or change server!</center>
-  </div>
 
-  <!--[ Streaming Player ]-->
-  <div id="shadow"></div>
-  <div class="DagPlaArea DagTo">
-    <div class="video-content" id="PlayVideo">
-      <div id="embed_holder">
-        <div class="player-embed" id="pembed">
-          <div class="playerload"></div>
-          <div id="player_embed">
-            <div class="pframe"><iframe allowfullscreen="true" frameborder="0" height="100%" marginheight="0" marginwidth="0" mozallowfullscreen="true" scrolling="NO" src="{server1}" webkitallowfullscreen="true" width="100%"></iframe>
-            </div>
+<div class="server_option" style="font-family:'Poppins', sans-family;font-weight:100px;">
+  <center>If the video doesn't appear<br/>refresh the page or change server!</center>
+</div>
+
+
+<!--[ Streaming Player ]-->
+<div id="shadow"></div>
+<div class="DagPlaArea DagTo">
+  <div class="video-content" id="PlayVideo">
+    <div id="embed_holder">
+      <div class="player-embed" id="pembed">
+        <div class="playerload"></div>
+        <div id="player_embed">
+          <div class="pframe"><iframe allowfullscreen="true" frameborder="0" height="100%" marginheight="0" marginwidth="0" mozallowfullscreen="true" scrolling="NO" src="PASTE" webkitallowfullscreen="true" width="100%"></iframe>
           </div>
         </div>
       </div>
     </div>
-    <div class="video-nav">
-      <div class="itemleft">
-        <div class="icon DagLight">
-          <svg viewbox="0 0 24 24">
-            <path d="M20,11H23V13H20V11M1,11H4V13H1V11M13,1V4H11V1H13M4.92,3.5L7.05,5.64L5.63,7.05L3.5,4.93L4.92,3.5M16.95,5.63L19.07,3.5L20.5,4.93L18.37,7.05L16.95,5.63M12,6A6,6 0 0,1 18,12C18,14.22 16.79,16.16 15,17.2V19A1,1 0 0,1 14,20H10A1,1 0 0,1 9,19V17.2C7.21,16.16 6,14.22 6,12A6,6 0 0,1 12,6M14,21V22A1,1 0 0,1 13,23H11A1,1 0 0,1 10,22V21H14M11,18H13V15.87C14.73,15.43 16,13.86 16,12A4,4 0 0,0 12,8A4,4 0 0,0 8,12C8,13.86 9.27,15.43 11,15.87V18Z" fill="currentColor">
-          </path></svg>
-          <span>Turn on Light</span>
-        </div>
-        <div class="icon DagShre">
-          <svg aria-hidden="true" height="1em" preserveaspectratio="xMidYMid meet" role="img" viewbox="0 0 24 24" width="1em" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g fill="none"><path d="M3 3h6v2H5v4H3V3z" fill="currentColor"><path d="M3 21h6v-2H5v-4H3v6z" fill="currentColor"><path d="M15 21h6v-6h-2v4h-4v2z" fill="currentColor"><path d="M21 3h-6v2h4v4h2V3z" fill="currentColor"></path></path></path></path></g></svg>
-          <a href="#PlayVideo">Expand</a>
-        </div>
+  </div>
+  <div class="video-nav">
+    <div class="itemleft">
+      <div class="icon DagLight">
+        <svg viewbox="0 0 24 24">
+          <path d="M20,11H23V13H20V11M1,11H4V13H1V11M13,1V4H11V1H13M4.92,3.5L7.05,5.64L5.63,7.05L3.5,4.93L4.92,3.5M16.95,5.63L19.07,3.5L20.5,4.93L18.37,7.05L16.95,5.63M12,6A6,6 0 0,1 18,12C18,14.22 16.79,16.16 15,17.2V19A1,1 0 0,1 14,20H10A1,1 0 0,1 9,19V17.2C7.21,16.16 6,14.22 6,12A6,6 0 0,1 12,6M14,21V22A1,1 0 0,1 13,23H11A1,1 0 0,1 10,22V21H14M11,18H13V15.87C14.73,15.43 16,13.86 16,12A4,4 0 0,0 12,8A4,4 0 0,0 8,12C8,13.86 9.27,15.43 11,15.87V18Z" fill="currentColor">
+        </path></svg>
+        <span>Turn on Light</span>
       </div>
-      <div class="itemright">
-      <div class="icon Report">
-          <svg aria-hidden="true" height="1em" preserveaspectratio="xMidYMid meet" role="img" viewbox="0 0 192 512" width="0.38em" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><path d="M176 432c0 44.112-35.888 80-80 80s-80-35.888-80-80s35.888-80 80-80s80 35.888 80 80zM25.26 25.199l13.6 272C39.499 309.972 50.041 320 62.83 320h66.34c12.789 0 23.331-10.028 23.97-22.801l13.6-272C167.425 11.49 156.496 0 142.77 0H49.23C35.504 0 24.575 11.49 25.26 25.199z" fill="currentColor"></path></svg>
-          <a href="https://t.me/JavUncensoredXx" target="_blank">Join Telegram!</a>
-        </div>
+      <div class="icon DagShre">
+        <svg aria-hidden="true" height="1em" preserveaspectratio="xMidYMid meet" role="img" viewbox="0 0 24 24" width="1em" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g fill="none"><path d="M3 3h6v2H5v4H3V3z" fill="currentColor"><path d="M3 21h6v-2H5v-4H3v6z" fill="currentColor"><path d="M15 21h6v-6h-2v4h-4v2z" fill="currentColor"><path d="M21 3h-6v2h4v4h2V3z" fill="currentColor"></path></path></path></path></g></svg>
+        <a href="#PlayVideo">Expand</a>
+      </div>
+    </div>
+    <div class="itemright">
+    <div class="icon Report">
+        <svg aria-hidden="true" height="1em" preserveaspectratio="xMidYMid meet" role="img" viewbox="0 0 192 512" width="0.38em" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><path d="M176 432c0 44.112-35.888 80-80 80s-80-35.888-80-80s35.888-80 80-80s80 35.888 80 80zM25.26 25.199l13.6 272C39.499 309.972 50.041 320 62.83 320h66.34c12.789 0 23.331-10.028 23.97-22.801l13.6-272C167.425 11.49 156.496 0 142.77 0H49.23C35.504 0 24.575 11.49 25.26 25.199z" fill="currentColor"></path></svg>
+        <a href="https://t.me/JavUncensoredXx" target="_blank">Join Telegram!</a>
       </div>
     </div>
   </div>
+</div>
 
 
-  <!--[ Streaming Server ]-->
-  <center>
-  <div class="server_option" style="padding:1px;">
-    <div class="tabs servers">
-      <span class="tablinks" id="serverOpen" onclick="openCity(event, 'VideoVard')"></span>
-    </div>
-    <div id="server">
-      <ul id="VideoVard" class="serverEpisode">
-        <li>
-          <div class="DagPlayOpt" data-embed="{server1}" data-id="Server-1">
-            <span>Server 1</span> </div>
-        </li>
-        <li>
-          <div class="DagPlayOpt" data-embed="{server2}" data-id="Server-1">
-            <span>Server 2</span> </div>
-        </li>
-      </ul>
-    </div>
-  </div></center>
-  """
-
-  
-  await message.reply(TEXT_MSG)
+<!--[ Streaming Server ]-->
+<center>
+<div class="server_option" style="padding:1px;">
+  <div class="tabs servers">
+    <span class="tablinks" id="serverOpen" onclick="openCity(event, 'VideoVard')"></span>
+  </div>
+  <div id="server">
+    <ul id="VideoVard" class="serverEpisode">
+      <li>
+        <div class="DagPlayOpt" data-embed="PASTE" data-id="Server-1">
+          <span>Server 1</span> </div>
+      </li>
+      <li>
+        <div class="DagPlayOpt" data-embed="PASTE" data-id="Server-1">
+          <span>Server 2</span> </div>
+      </li>
+    </ul>
+  </div>
+</div></center>
