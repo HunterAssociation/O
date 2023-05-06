177264f219b3f5783e6ec0f3138ccbe7e876f2b1
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-05-06 23:25:39 +0700

    Update start.py

--- Geonime/start.py
+++ Geonime/start.py
@@ -3,9 +3,8 @@ from pyrogram.types import *
 from bot import app
 
 
-@Client.on_message(filters.command("start"))
+@Client.on_message(filters.command("post_season"))
 async def start(client, message):
-  await message.reply("Halo !")
-  answer = await message.chat.ask('*Send me your name:*', parse_mode=enums.ParseMode.MARKDOWN)
-  await answer.request.edit_text("Name received!")
-  await answer.reply(f'Your name is: {answer.text}', quote=True)
+  title = await message.chat.ask('*Masukkan Judul:*', parse_mode=enums.ParseMode.MARKDOWN)
+  studio = await message.chat.ask('*Masukkan Studio:*', parse_mode=enums.ParseMode.MARKDOWN)
+  await message.reply(f'Judul: {title}\nStudio: {studio}', quote=True)
